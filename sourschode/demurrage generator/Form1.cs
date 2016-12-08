1c0a854272d5ad62501299ffd0abd3d56fe734aa
Šimon B. Gold <simon.b.gold@gmail.com>
2016-12-08 20:33:29 +0100

    yis

---
+++ sourschode/demurrage generator/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace demurrage_generator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Visible = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        { //Error
+            MessageBox.Show("there was an error it appears you cannot be sent any demurrage at this time", "Error");
+        }
+    }
+}
